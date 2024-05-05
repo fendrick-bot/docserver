34791003de6bde9823a4be85367bcfc4338816fd
fendrick <its.fendrick@example.com>
2024-05-05 10:04:13 +0530

    add secure link

--- app/saved/page.js
+++ app/saved/page.js
@@ -2,7 +2,7 @@ export default function saved() {
   return (
     <main>
       <br /><br /><br /><br />
-      <embed src='http://res.cloudinary.com/djtt5oivu/image/upload/v1714826111/a3mjkvu0htz0uwu8m2si.pdf' type="application/pdf"  width= "100%" height= "650px" title="New Pdf" style={{background:"white"}}/>
+      <embed src='https://res.cloudinary.com/djtt5oivu/image/upload/v1714826111/a3mjkvu0htz0uwu8m2si.pdf' type="application/pdf"  width= "100%" height= "650px" title="New Pdf" style={{background:"white"}}/>
       {/* <iframe
         src="http://res.cloudinary.com/djtt5oivu/image/upload/v1714826111/a3mjkvu0htz0uwu8m2si.pdf"
         frameborder="0"
