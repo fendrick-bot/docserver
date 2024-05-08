1d391583eda40cd0c65e2b013eb5c38560a2d2d0
fendrick <its.fendrick@example.com>
2024-05-08 10:10:02 +0530

    responsive splash

--- comps/GetStarted/GetStarted.js
+++ comps/GetStarted/GetStarted.js
@@ -1,43 +1,56 @@
-'use client'
+"use client";
 import "@/comps/GetStarted/GetStartedStyle.css";
 import { TbFileText } from "react-icons/tb";
 
 import Image from "next/image";
-import mainLogo from "@/public/mainlogo.png"
+import mainLogo from "@/public/mainlogo.png";
 
 import { useState, useEffect } from "react";
 import { TopNav } from "../TopNav/TopNav";
 import { BottomNav } from "../BottomNav/BottomNav";
 
-export function GetStarted(props){
-    console.log(props.data);
-    const [newUser, setUser] = useState(true);
-    useEffect(()=>{
-        if(localStorage.getItem("newUser") != null){
-            setUser(false);
-        }
-    }, []);
-    function removeSplash(){
-        localStorage.setItem("newUser", false);
-        setUser(false);
+export function GetStarted(props) {
+  console.log(props.data);
+  const [newUser, setUser] = useState(true);
+  useEffect(() => {
+    if (localStorage.getItem("newUser") != null) {
+      setUser(false);
     }
-    return(
-        (newUser)?<div id="splash-page">
-            <div id="big-logo">
-                <Image src={mainLogo} unoptimized/> 
-            </div>
-            <br />
-            <br />
-            <h1 id="splash-sub-head">Share files easily with  your friends</h1>
-            <p id="splash-para-text">Cloud Panda provides you the best services for uploading your files and share them with your friends</p>
+  }, []);
+  function removeSplash() {
+    localStorage.setItem("newUser", false);
+    setUser(false);
+  }
+  return newUser ? (
+    <div id="splash-page">
+      <div id="big-logo">
+        <Image
+          src={mainLogo}
+          alt="main logo"
+          width={0}
+          height={400}
+          style={{width:'100%'}}
+          unoptimized
+          priority
+        />
+      </div>
+      <br />
+      <br />
+      <h1 id="splash-sub-head">Share files easily with your friends</h1>
+      <p id="splash-para-text">
+        Cloud Panda provides you the best services for uploading your files and
+        share them with your friends
+      </p>
 
-            <button id="splash-start-button" onClick={removeSplash}>Get Started ▶️</button>
-
-        </div>:
-        <div>
-            <TopNav/>
-            {props.data}
-            <BottomNav/>
-        </div>
-    )
+      <button id="splash-start-button" onClick={removeSplash}>
+        Get Started ▶️
+      </button>
+    </div>
+  ) : (
+    <div>
+      <TopNav />
+      {props.data}
+      <BottomNav />
+    </div>
+  );
 }
